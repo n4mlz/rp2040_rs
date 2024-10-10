e671e54b3ec38769e25fde43092f69ea579a1ce0
n4mlz <n4mlz.dev@gmail.com>
2024-10-10 14:08:08 +0900

    :zap: コメントを日本語に

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-//! XIAO RP2040 Blinking LED Example
+//! XIAO RP2040 LED点滅の例
 //!
 //! https://tutoduino.fr/
 //!
-//! Blinks the LED on a Seeed Studio XIAO RP2040 board.
+//! Seeed Studio XIAO RP2040ボード上のLEDを点滅させます。
 //!
 #![no_std]
 #![no_main]
@@ -12,23 +12,21 @@ use seeeduino_xiao_rp2040::entry;
 use seeeduino_xiao_rp2040::hal;
 use seeeduino_xiao_rp2040::hal::pac;
 use seeeduino_xiao_rp2040::hal::prelude::*;
-/// Entry point to our bare-metal application.
+/// ベアメタルアプリケーションへのエントリーポイント。
 ///
-/// The `#[entry]` macro ensures the Cortex-M start-up code calls this function
-/// as soon as all global variables are initialised.
+/// `#[entry]`マクロは、グローバル変数の初期化が完了したら、Cortex-Mのスタートアップコードがこの関数を呼び出すようにします。
 ///
-/// The function configures the RP2040 peripherals, then blinks the LED in an
-/// infinite loop.
+/// 関数はRP2040の周辺機器を設定し、その後無限ループでLEDを点滅させます。
 #[entry]
 fn main() -> ! {
-    // Grab our singleton objects
+    // シングルトンオブジェクトを取得します
     let mut pac = pac::Peripherals::take().unwrap();
     let core = pac::CorePeripherals::take().unwrap();
-    // Set up the watchdog driver - needed by the clock setup code
+    // ウォッチドライバをセットアップします - クロック設定コードに必要です
     let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
-    // Configure the clocks
+    // クロックを設定します
     //
-    // The default is to generate a 125 MHz system clock
+    // デフォルトでは、125 MHzのシステムクロックを生成します
     let clocks = hal::clocks::init_clocks_and_plls(
         seeeduino_xiao_rp2040::XOSC_CRYSTAL_FREQ,
         pac.XOSC,
@@ -40,30 +38,29 @@ fn main() -> ! {
     )
     .ok()
     .unwrap();
-    // The single-cycle I/O block controls our GPIO pins
+    // シングルサイクルI/OブロックがGPIOピンを制御します
     let sio = hal::Sio::new(pac.SIO);
-    // Set the pins up according to their function on this particular board
+    // この特定のボードでの機能に従ってピンを設定します
     let pins = seeeduino_xiao_rp2040::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
-    // The delay object lets us wait for specified amounts of time (in
-    // milliseconds)
+    // delayオブジェクトを使って指定された時間（ミリ秒単位）待機します
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
-    // Configure the pins to operate as a push-pull output
+    // ピンをプッシュプル出力として動作させるように設定します
     let mut led_blue_pin = pins.led_blue.into_push_pull_output();
     let mut led_green_pin = pins.led_green.into_push_pull_output();
     let mut led_red_pin = pins.led_red.into_push_pull_output();
-    // Set green and red led OFF
+    // 緑と赤のLEDをオフに設定します
     led_green_pin.set_high().unwrap();
     led_red_pin.set_high().unwrap();
     loop {
-        // Turn on blue LED for 1s
+        // 青のLEDを1秒間点灯させます
         led_blue_pin.set_low().unwrap();
         delay.delay_ms(1000);
-        // Turn off blue LED for 500ms
+        // 青のLEDを500ミリ秒間消灯します
         led_blue_pin.set_high().unwrap();
         delay.delay_ms(500);
     }
